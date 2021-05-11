c8594c4b81950a7f495a9db89621a9c38ba17e7b
Vladimir <vtitov@nomia.net>
2021-05-11 12:49:31 +0300

    Added Defining bean RabbitTemplate

--- src/main/java/ru/titov/axonappexample/config/AxonConfig.java
+++ src/main/java/ru/titov/axonappexample/config/AxonConfig.java
@@ -9,6 +9,8 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,6 +26,11 @@ public class AxonConfig {
     }
 
     private static final String AXON_QUEUE = "axonAppQueue";
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        return new RabbitTemplate(connectionFactory);
+    }
 
     @Bean
     public Queue queue() {
